c5e5ca7f2985174af93346372ef43b4cbf862aef
Paulo Vieira Centeno <paulo.vieira.centeno@outlook.com>
2020-08-03 19:24:50 -0300

    First commit

---
+++ simple_channel/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+)
+
+func greet(c chan string) {
+	fmt.Println("Hello " + <-c + "!")
+}
+
+func main() {
+	fmt.Println("main() started")
+	c := make(chan string)
+
+	go greet(c)
+	c <- "John"
+
+	go greet(c)
+	c <- "Peter"
+
+	fmt.Println("main() stopped")
+}
