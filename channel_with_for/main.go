0e49e19eec695de95296c0910b73c300239defb9
Paulo Vieira Centeno <paulo.vieira.centeno@outlook.com>
2020-08-04 17:18:27 -0300

    Go routines channel exampel with for loop

---
+++ channel_with_for/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+)
+
+func squares(c chan int) {
+	for i := 0; i < 10; i++ {
+		c <- i * i
+	}
+	close(c)
+}
+
+func main() {
+	fmt.Println("main() started")
+
+	c := make(chan int)
+
+	go squares(c)
+
+	for val := range c {
+		fmt.Println(val)
+	}
+
+	fmt.Println("main() stopped")
+}
